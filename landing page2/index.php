9bbe3222fbc54b161e26476449ea10cca2707e1d
Oddjobser <76171641+oddsjobser@users.noreply.github.com>
2021-03-20 18:25:55 +0100

    Add files via upload

---
+++ landing page2/index.php
@@ -0,0 +1,72 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Landing Page | differ</title>
+	<link rel="stylesheet" href="css/main.css">
+	<meta name="viewport" content="width=device-width, initiale-scale=1">
+</head>
+<body>
+	<div class="header">
+		<h1>Hello World</h1>
+		<a href="#index">Home</a>
+		<a href="#PageA">PageA</a>
+		<a href="#PageB">PageB</a>
+	</div>
+
+	<div class="row" id="PageA">
+		<div class="col-3 col-s-3 menu">
+			<ul>
+				<li>text</li>
+				<li>text</li>
+				<li>text</li>
+				<li>text</li>
+			</ul>
+		</div>
+
+		<div class="col-6 col-s-9">
+			<h1>Text</h1>
+			<p>Some blatant text that needs to take up some space so I can test whatever is going on
+			on here. This is some nice way to make webpages</p>
+		</div>
+
+		<div class="col-3 col-s-12">
+			<div class="aside">
+				<h2>What?</h2>
+				<p>something something world</p>
+				<h2>Where?</h2>
+				<p>something something world</p>
+				<h2>How?</h2>
+				<p>something something world</p>
+			</div>
+		</div>
+	</div>
+
+	<div class="row" id="PageB">
+		<div class="col-12 col-s12 section">
+			<div class="center images">
+				<div class="box">
+					<img src="https://wallup.net/wp-content/uploads/2019/05/10/338365-mountains-landscapes-nature-lone-tree-natural-scenery-pure-blue.jpg">
+					<p>Some text here about dinosaurus concurring the world, but they couldn't you know why? because they are 
+					extint lole</p>
+				</div>
+				<div class="box">
+					<img src="https://wallup.net/wp-content/uploads/2019/05/10/338365-mountains-landscapes-nature-lone-tree-natural-scenery-pure-blue.jpg">
+					<p>Some text here about dinosaurus concurring the world, but they couldn't you know why? because they are 
+					extint lole</p>
+				</div>
+				<div class="box">
+					<img src="https://wallup.net/wp-content/uploads/2019/05/10/338365-mountains-landscapes-nature-lone-tree-natural-scenery-pure-blue.jpg">
+					<p>Some text here about dinosaurus concurring the world, but they couldn't you know why? because they are 
+					extint lole</p>
+				</div>
+			</div>
+		</div>
+	</div>
+
+	
+
+	<div class="footer">
+		<p>Resize the browser window</p>
+	</div>
+</body>
+</html>
